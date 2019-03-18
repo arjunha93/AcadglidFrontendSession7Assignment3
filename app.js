8a5c1738813cd4f7f417661bce1209e34e8251d8
arjunha93 <48612246+arjunha93@users.noreply.github.com>
2019-03-18 15:17:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-var arr = [12,34,4,5,6,7,8,9,44];
-var sum = 0;
-function getSum(arr){
-    for(var i = 0; i<arr.length;i++){
-        sum = sum + arr[i];
-    }
-    console.log("Sum : " + sum);
+var count = 1;
+function counter(num){
+    num++;
+    count = num;
+    console.log("count : " + num);
 }  
 
-getSum(arr);
+counter(count);
